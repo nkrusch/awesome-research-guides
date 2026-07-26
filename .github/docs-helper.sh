ba74517cebad13caf3f0016a5a1daa70a8a50323
Neea <2580981+nkrusch@users.noreply.github.com>
2026-07-27 01:24:54 +0200

    docs and workflow maintenance (#33)

--- .github/docs-helper.sh
+++ .github/docs-helper.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-
 cd "$(git rev-parse --show-toplevel)" || return
 
 SOURCE="readme.md"
+CONTRIB=".github/contributing.md"
 MEDIA="assets"
 OUT="docs"
 
 mkdir -p "$OUT" "$OUT"/$MEDIA
 cp -f "$MEDIA"/*.png "$OUT"/$MEDIA 2>/dev/null
 cp -f "$MEDIA"/*.css "$OUT" 2>/dev/null
-cp -f .github/contributing.md "$OUT"/contributing.md
+cp -f "$CONTRIB" "$OUT"/contributing.md
 
 awk -v out_dir="$OUT" '
   BEGIN {
@@ -32,7 +32,6 @@ awk -v out_dir="$OUT" '
     clean_title = tolower(title_text);
     gsub(/[ \t]+/, "_", clean_title);
     gsub(/[^a-z0-9_]/, "", clean_title);
-
     f = sprintf("%s/%02d_%s.md", out_dir, count, clean_title);
     print "---" > f;
     print "title: " title_text > f;
@@ -45,25 +44,3 @@ awk -v out_dir="$OUT" '
     }
   }
 ' "$SOURCE"
-
-awk '
-  /^> \[!(NOTE|TIP|IMPORTANT|WARNING|CAUTION)\]/ {
-    # Match the pattern to set RSTART and RLENGTH
-    match($0, /\[![A-Z]+\]/);
-    label = tolower(substr($0, RSTART + 2, RLENGTH - 3));
-    if (label == "caution")   label = "danger";
-    if (label == "important") label = "info";
-   print "!!! " label;
-    in_alert = 1;
-    next;
-  }
-  in_alert && /^>/ {
-    sub(/^>[[:space:]]?/, "    ");
-    print $0;
-    next;
-  }
-  !/^>/ {
-    in_alert = 0;
-  }
-  { print $0; }
-' ".github/contributing.md" > "$OUT/contributing.md"
